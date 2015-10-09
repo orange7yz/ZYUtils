939ebff6d43955f2318ced6fda9f1ed7a9b02908
zhangy <lebron3288@sina.com>
2015-10-09 16:38:41 +0800

    Initial Commit

---
+++ ZYUtils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYUtils
+//
+//  Created by yongzhang on 15/10/9.
+//  Copyright © 2015年 yongzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
